20793d37d686ec38a80491d72ccf74e2187a525c
lak7 <lakshaygupta2511@gmail.com>
2026-03-21 18:51:29 +0530

    Done till stopword filter

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -1,10 +1,35 @@
-from lib.load_data import load_data
+from lib.load_data import load_data, load_stopwords
+import string
+
+def clean_text(txt):
+    txt = txt.lower()
+    txt = txt.translate(str.maketrans("", "", string.punctuation))
+    return txt
+
+def tokenize_text(txt):
+    txt = clean_text(txt).split()
+    stopwords = load_stopwords()
+    tokens = []
+    for tok in txt:
+        if tok not in stopwords:
+            tokens.append(tok)
+    return tokens
+
+def has_matching_token(query_token, movie_token):
+    for qTok in query_token:
+        for mTok in movie_token:
+            if qTok in mTok:
+                return True
+    return False 
 
 def search_query(query):
     results = []
+    queryToken = tokenize_text(query)
+    print(f"This is the query token: {queryToken}")
     movies = load_data()
     for movie in movies:
-        if query.lower() in movie["title"].lower():
+        movieToken = tokenize_text(movie["title"])
+        if has_matching_token(query_token=queryToken, movie_token=movieToken):
             results.append(movie["title"])
     return results
     
--- cli/lib/load_data.py
+++ cli/lib/load_data.py
@@ -5,3 +5,7 @@ def load_data():
     with open("data/movies.json", "r", encoding="utf-8") as f:
         dataDic = json.load(f)
     return dataDic["movies"]
+
+def load_stopwords():
+    with open("data/stopwords.txt", "r") as f:
+        return {line.strip() for line in f if line.strip()}
