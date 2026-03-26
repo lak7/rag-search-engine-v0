3bd89d987723044874e0ff9c3be6e7d402c4b7aa
lak7 <lakshaygupta2511@gmail.com>
2026-03-26 23:03:16 +0530

    Started implementing semantic from scratch

---
+++ cli/lib/semantic_search.py
@@ -0,0 +1,10 @@
+from sentence_transformers import SentenceTransformer
+
+class SemanticSearch:
+    def __init__(self) -> None:
+        self.model = SentenceTransformer('all-MiniLM-L6-v2')
+
+def verify_model():
+    semanticSearch = SemanticSearch()
+    print(f"Model loaded: {semanticSearch.model}")
+    print(f"Max sequence length: {semanticSearch.model.max_seq_length}")
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,11 +2,20 @@
 
 import argparse
 
+from lib.semantic_search import verify_model
+
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
+    subparsers = parser.add_subparsers(dest="command", help="Available commands")
+
+    subparsers.add_parser("verify", help="Verifies if embedding model loads properly")
+
+
     args = parser.parse_args()
 
     match args.command:
+        case "verify":
+            verify_model()
         case _:
             parser.print_help()
 
