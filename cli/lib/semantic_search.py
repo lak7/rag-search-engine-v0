13e26d9b3842bcc59e9d780ab9105a905186dc96
lak7 <lakshaygupta2511@gmail.com>
2026-04-01 20:59:29 +0530

    Embedded all documents and caching

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -1,13 +1,45 @@
+import numpy as np
 from sentence_transformers import SentenceTransformer
+
+from lib.common import CACHE_PATH
+from lib.load_data import load_data
 
 class SemanticSearch:
     def __init__(self) -> None:
         self.model = SentenceTransformer('all-MiniLM-L6-v2')
+        self.embeddings = None
+        self.documents = None
+        self.document_map = {}
+        self.embeddings_path = CACHE_PATH / 'movie_embeddings.npy'
     
     def generate_embedding(self, text):
         if (text is None) or (text == ""):
             return ValueError("Text cannot be None or empty")
         return (self.model.encode([text]))[0]
+    
+    def build_embeddings(self, document):
+        self.documents = document
+        self.document_map = {}
+        movie_strings = []
+        for doc in self.documents:
+            self.document_map[doc['id']] = doc
+            movie_strings.append(f"{doc['title']} {doc['description']}")
+        self.embeddings = self.model.encode(movie_strings, show_progress_bar=True)
+        np.save(self.embeddings_path, self.embeddings)
+        return self.embeddings
+
+    def load_or_create_embeddings(self, documents):
+        self.documents = documents
+        self.document_map = {}
+        for doc in self.documents:
+            self.document_map[doc['id']] = doc
+
+        if self.embeddings_path.exists():
+            self.embeddings = np.load(self.embeddings_path)
+            if len(self.embeddings) == len(documents):
+                return self.embeddings
+
+        return self.build_embeddings(documents)
 
 def verify_model():
     semanticSearch = SemanticSearch()
@@ -20,3 +52,11 @@ def embed_text(text):
     print(f"Text: {text}")
     print(f"First 3 dimensions: {embedding[:3]}")
     print(f"Dimensions: {embedding.shape[0]}")
+
+
+def verify_embeddings():
+    semanticSearch = SemanticSearch()
+    documents = load_data()
+    embeddings = semanticSearch.load_or_create_embeddings(documents)
+    print(f"Number of docs:   {len(documents)}")
+    print(f"Embeddings shape: {embeddings.shape[0]} vectors in {embeddings.shape[1]} dimensions")
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -2,7 +2,7 @@
 
 import argparse
 
-from lib.semantic_search import embed_text, verify_model
+from lib.semantic_search import embed_text, verify_embeddings, verify_model
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
@@ -13,6 +13,7 @@ def main():
     embed_text_parser = subparsers.add_parser("embed_text", help="Embeds text into a vector")
     embed_text_parser.add_argument("text", type=str, help="Text to embed")
 
+    subparsers.add_parser("verify_embeddings", help="Verifies cached movie embeddings")
  
     args = parser.parse_args()
 
@@ -21,6 +22,8 @@ def main():
             verify_model()
         case "embed_text":
             embed_text(args.text)
+        case "verify_embeddings":
+            verify_embeddings()
         case _:
             parser.print_help()
 
